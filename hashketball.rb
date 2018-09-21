c46cbe4720370e91595b753b340158518f3b6855
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-21 15:53:27 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -198,3 +198,17 @@ def player_stats(player_name)
   end
   player_stats
 end
+
+def big_shoe_rebounds
+  big_shoes_guy = 0
+  rebounds = 0
+    game_hash.each do | team, values|
+      values[:players].each do | stats |
+        if stats[:shoe] > big_shoes_guy
+          big_shoes_guy = stats[:shoe]
+          rebounds = stats[:rebounds]
+        end
+      end
+    end
+  rebounds
+end
