26060cbd050dc6284143a1cfec26e5b26bd7969d
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-21 15:43:12 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -192,7 +192,7 @@ def player_stats(player_name)
     values[:players].each do |stats|
       if stats[:player] == player_name
         stats.delete(:player)
-        stats = player_stats.to_i
+        stats = player_stats
       end
     end
   end
