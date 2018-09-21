d2ec86e325c3073f8e8f64c4f5b075494b8e9231
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-21 15:31:46 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -175,7 +175,7 @@ def player_numbers(team_name)
   game_hash.each do |team, stat|
     if stat[:team_name] == team_name
       stat[:players].each do |player|
-        player.each do |key, value|
+        players.each do |key, value|
           if key == :number
             player_list << value
           end
